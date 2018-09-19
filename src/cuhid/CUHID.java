294859eed66f12de61bc7855b2d76ad4959f8078
DTCMOTHERSHIP\Pc <evega-12@hotmail.com>
2018-09-19 18:09:40 -0500

    Cambiado header de codigo

--- src/cuhid/CUHID.java
+++ src/cuhid/CUHID.java
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Este programa ha sido creado por Erick Renato Vega Cerón para el grupo 17 de Ingeniería en Sistemas Computacionales
+//Centro Universitario Hidalguense
+//erick@armony.systems
+
 package cuhid;
 import java.io.IOException;
 import com.fazecast.jSerialComm.SerialPort;
